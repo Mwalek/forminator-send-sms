c1dc5b137f6126c4abbc1447821362f94b3d74ce
Mwale Kalenga <mwale.kalenga@prosites.space>
2022-08-28 12:16:16 +0200

    Require composer autoload once.

--- forminator-send-sms.php
+++ forminator-send-sms.php
@@ -25,7 +25,7 @@
  * Domain Path:       /languages
  */
 
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
